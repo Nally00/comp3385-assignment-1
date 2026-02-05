714b347c0372b9f8d4b719937e0dd79c25415dc1
Naleka <nalekabeckford@gmail.com>
2026-02-05 02:33:49 -0400

    Adding image and styling

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,6 @@
 
 @section('content')
   <h2>Naleka Beckford</h2>
-  <p>Naleka is a third year computer science major at the University of the West Indies Five Islands Campus.</p>
-@endsection
+  <p>Naleka is a third year computer science major at the University of the West Indies Five Islands Campus. She is interested in learning new web technologies, and gaining a better understanding of frameworks.</p>
+  <img src="{{ url('/images/graphic.jpg') }}" alt="Girl on Computer" />
+  @endsection
